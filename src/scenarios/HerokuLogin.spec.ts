eaa6998e982374b1a9edc46c5d4caea4acd31cf1
Henrique Forgiarini Da Silva <henrique.202917@alunosatc.edu.br>
2025-11-04 20:21:56 -0300

    teste

---
+++ src/scenarios/HerokuLogin.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Login no site HerokuApp', () => {
+  test('Deve realizar login com sucesso', async ({ page }) => {
+    // ...
+  });
+});
